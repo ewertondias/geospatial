55de2204be95529bca68d378973a051bb42162ef
Ewerton Dias <ewertondsdias@gmail.com>
2026-01-22 11:15:26 -0300

    feat: Add handler for IllegalArgumentException

--- src/main/java/com/sccon/geospatial/infrastructure/handler/GlobalExceptionHandler.java
+++ src/main/java/com/sccon/geospatial/infrastructure/handler/GlobalExceptionHandler.java
@@ -4,10 +4,12 @@ import org.jspecify.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -40,6 +42,17 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         problemDetail.setProperty("errors", errors);
 
         return ResponseEntity.status(status)
+            .body(problemDetail);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ProblemDetail> handleIllegalArgument(IllegalArgumentException ex) {
+        log.error("Illegal argument: {}", ex.getMessage());
+
+        var httpStatus = HttpStatus.BAD_REQUEST;
+        var problemDetail = ProblemDetail.forStatusAndDetail(httpStatus, ex.getMessage());
+
+        return ResponseEntity.status(httpStatus)
             .body(problemDetail);
     }
 
